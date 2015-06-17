95b40a4c2746faf2b3b9c1fef537026beda978ec
Jay C <imjching@hotmail.com>
2015-06-17 18:13:59 +0800

    active record migration (wed)

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - self.birthday.year - (self.birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
 end
